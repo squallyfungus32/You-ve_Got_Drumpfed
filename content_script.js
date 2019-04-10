a4dff16fe7e3dd36c6201f52add6f1cf225285b0
Andrew McGrice <andrew@pamdrew.ninja>
2019-04-11 09:41:12 +1000

    remove font, cleaned up replacements

--- content_script.js
+++ content_script.js
@@ -1,11 +1,3 @@
-/*var styleNode = document.createElement ("style");
-styleNode.type = "text/css";
-styleNode.textContent = "@font-face { font-family: BFTinyHand-Regular; src: url('"
-                    + chrome.extension.getURL ("chrome-extension://__MSG_@@extension_id__/fonts/BFTinyHand-Regular.otf")
-                    + "'); }"
-                    ;
-document.head.appendChild (styleNode);*/
-
 walk(document.body);
 
 function walk(node)
@@ -37,34 +29,32 @@ function handleText(textNode)
 {
     var text = textNode.nodeValue;
 
-    text = text.replace(/TRUMP/, 'DRUMPF');
-    text = text.replace(/Trump/, 'Drumpf');
-    text = text.replace(/trump/, 'drumpf');
-//    text = text.replace(/\btrump\b/gi, 'drumpf');
-    text = text.replace(/Make America Great Again/gi, 'Make America Drumpf Again');
-    text = text.replace(/make America great again/gi, 'make America Drumpf again');
+    text = text.replace(/TRUMP/g, 'DRUMPF');
+    text = text.replace(/Trump/g, 'Drumpf');
+    text = text.replace(/trump/g, 'drumpf');
+    text = text.replace(/\btrump\b/g, 'drumpf');
+    text = text.replace(/Make America Great Again/g, 'Make America Drumpf Again');
+    text = text.replace(/make America great again/g, 'make America Drumpf again');
     text = text.replace(/MakeAmericaGreatAgain/gi, 'MakeAmericaDrumpfAgain');
-    text = text.replace(/Hamburger/gi, 'Hamberder');
-    text = text.replace(/hamburger/gi, 'hamberder');
-    text = text.replace(/burger/gi, 'berder');
-    text = text.replace(/Burger/gi, 'Berder');
+    text = text.replace(/burger/g, 'berder');
+    text = text.replace(/Burger/g, 'Berder');
     text = text.replace(/unprecedented/gi, 'unpresidented');
     text = text.replace(/Tim Cook/gi, 'Tim Apple');
     text = text.replace(/coffee/gi, 'covfefe');
     text = text.replace(/coverage/gi, 'covfefe');
     text = text.replace(/origins/gi, 'oranges');
     text = text.replace(/big league/gi, 'bigly');
-    text = text.replace(/big /, 'bigly ');
-    text = text.replace(/Big /, 'Bigly ');
-    text = text.replace(/huge/, 'yuge');
-    text = text.replace(/Huge/, 'Yuge');
+    text = text.replace(/big /g, 'bigly ');
+    text = text.replace(/Big /g, 'Bigly ');
+    text = text.replace(/huge/g, 'yuge');
+    text = text.replace(/Huge/g, 'Yuge');
     text = text.replace(/Nepal/gi, 'Nipple');
     text = text.replace(/Bhutan/gi, 'Button');
     text = text.replace(/counsel/gi, 'concel');
     text = text.replace(/honored/gi, 'honered');
     text = text.replace(/too great/gi, 'to great');
-    text = text.replace(/attacker/, 'attaker');
-    text = text.replace(/Attacker/, 'Attaker');
+    text = text.replace(/attacker/g, 'attaker');
+    text = text.replace(/Attacker/g, 'Attaker');
     text = text.replace(/Denmark/gi, 'Denmakr');
     text = text.replace(/San Bernardino/gi, 'San Bernadino');
     text = text.replace(/Colombia/gi, 'Columbia');
